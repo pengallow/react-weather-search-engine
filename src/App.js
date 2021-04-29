219aa159e7b9b34d02e9e98c30a47269aed82369
pengallow <cellorin001@gmail.com>
2021-04-29 11:04:16 -0400

    Added bootstrap css import to index.js
    Also created Weather.js and Weather.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        React Weather Search Engine
-      </h1>
+      <div className="container">
+        <h1>
+          React Weather Search Engine
+        </h1>
+        <Weather />
+        </div>
+        <footer>
+          <a href="https://github.com/pengallow/react-weather-search-engine">GitHub Repository</a>
+        </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+        <div className="weather">
+            This is the meat of the project.
+        </div>
+    );
+}
