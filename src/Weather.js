f7e07966c8541d5309223613186d6487703314f4
pengallow <cellorin001@gmail.com>
2021-05-07 10:05:01 -0400

    Moved elements within Weather.js.
    Added styling in Weather.css.

--- src/Weather.js
+++ src/Weather.js
@@ -7,22 +7,20 @@ export default function Weather() {
     return (
         <div className="weather container">
             <form>
-                <input type="text" placeholder="Enter a city" className="col-9"/>
-                <input type="submit" value="Search" className="col-3" />
+                <input type="text" placeholder="Enter a city" className="col-8 search-bar"/>
+                <input type="submit" value="Search" className="col-3 center-block" />
             </form>
             <h2 className="default-city">
                 Virginia Beach
             </h2>
-            <ul>
-                <li>
-                    <EasyDate />
-                </li>
-                <li>Weather condition will go here.</li>
-            </ul>
             <div className="row">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="temporary weather icon" />
-                    29ºC | ºF
+                    <ul>
+                        <li>
+                            <EasyDate />
+                        </li>
+                        <li>Weather condition will go here.</li>
+                    </ul>
                 </div>
                 <div className="col-6">
                     <ul>
@@ -30,6 +28,10 @@ export default function Weather() {
                     <li>Wind Speed goes here</li>
                     </ul>
                 </div>
+            </div>
+            <div className="icon-and-temp">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="temporary weather icon" />
+                29ºC | ºF
             </div>
         </div>
     );
